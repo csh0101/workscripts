09bb1f0f519128688e13032d284d348b9194d5c2
Random <csh031027asd@gmail.com>
2023-07-16 12:34:20 +0800

    Create goenv.sh

---
+++ goenv.sh
@@ -0,0 +1,7 @@
+export GO111MODULE=on
+export GOROOT=/usr/local/bin/go
+export GOPATH=/Users/csh0101/lib/go
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+export GOPROXY="https://goproxy.cn,direct"
+export CGO_ENABLED="1"
